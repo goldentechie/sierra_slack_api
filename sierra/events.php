5e780c98fc39632a7b8b9b4f3c610b5b60be2ba1
goldentechie <bendam1207@gmail.com>
2021-11-17 18:20:32 -0500

    change slack hook url - 7

--- sierra/events.php
+++ sierra/events.php
@@ -1,18 +1,19 @@
 <?php
-  require(__DIR__ ."/eventHandlers/leadCreated.php");
+echo __DIR__;
+  // require(__DIR__ ."/eventHandlers/leadCreated.php");
 
-  // record all events to data.txt
-  $file = fopen("data.txt","a+");
-  $requestBody = file_get_contents('php://input');
-  $event = json_decode($requestBody);
-  fwrite($file, date('Y-m-d H:i:s') ."\n");
-  fwrite($file, $requestBody);
-  fwrite($file,"\n");
-  fwrite($file,"\n");
+  // // record all events to data.txt
+  // $file = fopen("data.txt","a+");
+  // // $requestBody = file_get_contents('php://input');
+  // $event = json_decode($requestBody);
+  // // fwrite($file, date('Y-m-d H:i:s') ."\n");
+  // // fwrite($file, $requestBody);
+  // // fwrite($file,"\n");
+  // // fwrite($file,"\n");
 
-  switch($event->eventType)
-  {
-    case "LeadCreated":
-      onLeadCreated($event);
-      break;
-  }
+  // switch($event->eventType)
+  // {
+  //   case "LeadCreated":
+  //     onLeadCreated($event);
+  //     break;
+  // }
