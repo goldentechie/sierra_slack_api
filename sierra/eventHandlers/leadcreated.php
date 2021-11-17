1849830e33cc34e9bed2800f50ddaa3ea6b1830b
goldentechie <bendam1207@gmail.com>
2021-11-17 15:49:50 -0500

    simple event-message feature

---
+++ sierra/eventHandlers/leadcreated.php
@@ -0,0 +1,21 @@
+<?php
+function onLeadCreated($event)
+{
+  // get Lead data
+  $cGetLead = curl_init("https://api.sierrainteractivedev.com/leads/get/".$event->resourceList[0]."?includeSavedSearches=true&includeTags=true&includeActionPlans=true");
+  curl_setopt($cGetLead, CURLOPT_HTTPHEADER, array("Content-Type:application/json", "Sierra-ApiKey:ba5d2b88-642d-4629-a2ca-30c3dcab34a5"));
+  $leadData = curl_exec($cGetLead);
+  curl_close($cGetLead);
+
+  // send a message to slack
+  require(__DIR__ . '/../../slack/apis/newlead.php');
+  
+  $cSendSlackMessage = curl_init("https://hooks.slack.com/services/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");
+  curl_setopt_array($cSendSlackMessage, array(
+    CURLOPT_HTTPHEADER=>array("Content-Type:application/json"),
+    CURLOPT_POST=>true,
+  ));
+  curl_setopt($cSendSlackMessage, CURLOPT_POSTFIELDS, new_lead_template($leadData));
+  var_dump(curl_exec($cSendSlackMessage));
+  curl_close($cSendSlackMessage);
+}
--- sierra/events.php
+++ sierra/events.php
@@ -1,11 +1,18 @@
 <?php
+  require(__DIR__ ."/eventHandlers/leadCreated.php");
+
+  // record all events to data.txt
   $file = fopen("data.txt","a+");
   $requestBody = file_get_contents('php://input');
   $event = json_decode($requestBody);
-  var_dump(date('Y-m-d'));
   fwrite($file, date('Y-m-d H:i:s') ."\n");
   fwrite($file, $requestBody);
   fwrite($file,"\n");
   fwrite($file,"\n");
-  var_dump($requestBody);
-  var_dump($event);
+
+  switch($event->eventType)
+  {
+    case "LeadCreated":
+      onLeadCreated($event);
+      break;
+  }
